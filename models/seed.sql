ff69d62bca6d52ec22ad09b1d483d270390cabf4
Carlos Quinto <carlquin77@gmail.com>
2019-06-03 13:40:06 -0700

    updated api routing, updated model files

--- models/seed.sql
+++ models/seed.sql
@@ -1,8 +1,20 @@
 USE contacts_db;
 CREATE table contacts (
-    id INT(60) NOT AUTO_INCREMENT,
+    contactId INT(20) NOT NULL AUTO_INCREMENT,
     nameFirst VARCHAR(20) NOT NULL,
     nameLast VARCHAR(20) NOT NULL,
     nameMiddle VARCHAR(20),
-    numPhone VARCHAR
-)
+    numPhone INT(10) NOT NULL,
+    streetAddress VARCHAR(20),
+    cityAddress VARCHAR(20),
+    stateAddress VARCHAR(3),
+    emailAddress VARCHAR(20),
+    PRIMARY KEY (contactId)
+);
+
+CREATE table photos (
+    photoId INT(30) NOT NULL AUTO_INCREMENT,
+    source VARCHAR(30),
+    PRIMARY KEY (photoID),
+    FOREIGN KEY (contactId)
+);
