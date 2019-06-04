d3c59df6b4c25f9091c026a505d1dab04fbb123f
Mackenzie <kenziecodes@gmail.com>
2019-06-04 12:02:56 -0700

    added actors

---
+++ models/contacts.js
@@ -0,0 +1,45 @@
+module.exports = function (sequelize, DataTypes) {
+  var Contact = sequelize.define("Contact", {
+    nameFirst : {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    nameLast : {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    nameMiddle: {
+      type: DataTypes.STRING,
+    },
+    numPhone: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    streetAddress: {
+      type: DataTypes.STRING,
+    },
+    cityAddress: {
+      type: DataTypes.STRING,
+    },
+    stateAddress: {
+      type: DataTypes.STRING,
+    },
+    zipCode: {
+      type: DataTypes.INTEGER,
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+    },
+    profession: {
+      type: DataTypes.STRING,
+    }
+  });
+
+  Contact.associate = function(models) {
+    // When a contact is deleted, also delete any associated Photos
+    Contact.hasMany(models.Photo, {
+      onDelete: "cascade"
+    });
+  };
+  return Contact;
+};
