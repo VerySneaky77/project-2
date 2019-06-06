73dc1cca2e265a595e075cfd95fb8e65bc12b723
Carlos Quinto <carlquin77@gmail.com>
2019-06-06 00:17:45 -0700

    completed add photo functionality with front-end and database working

--- models/contacts.js
+++ models/contacts.js
@@ -1,4 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
+// Contact object for sequelize to add to the database
   var Contact = sequelize.define("Contact", {
     nameFirst : {
       type: DataTypes.STRING,
